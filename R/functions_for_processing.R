59c15a1b0d858b5993cd6b559a20cac6a4145dcb
Noah Greifer <noah.greifer@gmail.com>
2018-07-13 00:33:46 -0400

    Added word.list

--- R/functions_for_processing.R
+++ R/functions_for_processing.R
@@ -272,3 +272,43 @@ is.formula <- function(f, sides = NULL) {
   }
   return(res)
 }
+word.list <- function(word.list = NULL, and.or = c("and", "or"), is.are = FALSE, quotes = FALSE) {
+  #When given a vector of strings, creates a string of the form "a and b"
+  #or "a, b, and c"
+  #If is.are, adds "is" or "are" appropriately
+  L <- length(word.list)
+  if (quotes) word.list <- sapply(word.list, function(x) paste0("\"", x, "\""))
+  if (L == 0) {
+    out <- ""
+    attr(out, "plural") = FALSE
+  }
+  else {
+    word.list <- word.list[!word.list %in% c(NA, "")]
+    L <- length(word.list)
+    if (L == 0) {
+      out <- ""
+      attr(out, "plural") = FALSE
+    }
+    else if (L == 1) {
+      out <- word.list
+      if (is.are) out <- paste(out, "is")
+      attr(out, "plural") = FALSE
+    }
+    else {
+      and.or <- match.arg(and.or)
+      if (L == 2) {
+        out <- paste(word.list, collapse = paste0(" ", and.or," "))
+      }
+      else {
+        out <- paste(paste(word.list[seq_len(L-1)], collapse = ", "),
+                     word.list[L], sep = paste0(", ", and.or," "))
+
+      }
+      if (is.are) out <- paste(out, "are")
+      attr(out, "plural") = TRUE
+    }
+
+
+  }
+  return(out)
+}
