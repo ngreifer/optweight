d5706f11d2339f7060a2bb97a2ba5baa4feba95e
Noah Greifer <noah.greifer@gmail.com>
2018-09-04 23:14:27 -0400

    Added check.targets; spelling errors and code cleaning

--- R/check.tols.R
+++ R/check.tols.R
@@ -1,6 +1,6 @@
 check.tols <- function(formula, data = NULL, tols, stop = FALSE) {
 
-  if (!is.formula(formula)) stop("The argument to formula must a single formula wit hthe covariates on the right side.", call. = FALSE)
+  if (!is.formula(formula)) stop("The argument to formula must a single formula with the covariates on the right side.", call. = FALSE)
   if (missing(tols)) tols <- NA_real_
   else if (!is.numeric(tols)) stop("tols must be a numeric vector.", call. = FALSE)
 
@@ -28,7 +28,7 @@ check.tols <- function(formula, data = NULL, tols, stop = FALSE) {
 
   if (is_null(model.covs)) stop("No covariates were specified.", call. = FALSE)
 
-  if (any(bad.covs <- !sapply(formula.covs, is.finite)) || nrow(formula.covs) != nrow(model.covs)) {
+  if (any(bad.covs <- sapply(formula.covs, function(x) any(!is.finite(x)))) || nrow(formula.covs) != nrow(model.covs)) {
     stop(paste0("No missing or non-finite values are allowed in the covariates. Missing or non-finite values were found in the following covariates:\n", paste(names(formula.covs)[bad.covs], collapse = ", ")), call. = FALSE)
   }
 
@@ -78,6 +78,73 @@ print.optweight.tols <- function(x, internal = FALSE, digits = 5, ...) {
       cat("\n- tols used internally by optweight:\n")
       print(round(internal.tols, digits))
     }
+  }
+  invisible(x)
+}
+
+check.targets <- function(formula, data = NULL, targets, stop = FALSE) {
+  if (!is.formula(formula)) stop("The argument to formula must a single formula with the covariates on the right side.", call. = FALSE)
+
+  #Process treat and covs from formula and data
+  tt <- delete.response(terms(formula))
+  t.c <- get.covs.and.treat.from.formula(tt, data)
+  formula.covs <- t.c[["reported.covs"]]
+  model.covs <- t.c[["model.covs"]]
+
+  formula.vars <- attr(attr(formula.covs, "terms"), "term.labels")
+  #formula.vars <- attr(tt, "term.labels")
+  if (is_null(formula.vars)) {
+    formula.vars <- "(Intercept)"
+    attr(model.covs, "assign") <- 1
+  }
+  model.vars <- colnames(model.covs)
+
+  if (is_null(model.covs)) stop("No covariates were specified.", call. = FALSE)
+
+  if (any(bad.covs <- sapply(formula.covs, function(x) any(!is.finite(x)))) || nrow(formula.covs) != nrow(model.covs)) {
+    stop(paste0("No missing or non-finite values are allowed in the covariates. Missing or non-finite values were found in the following covariates:\n", paste(names(formula.covs)[bad.covs], collapse = ", ")), call. = FALSE)
+  }
+
+  if (missing(targets)) targets <- setNames(rep(NA_real_, length(model.vars)), model.vars)
+  else if (!is.numeric(targets)) stop("targets must be a numeric vector.", call. = FALSE)
+
+  if (length(targets) != length(model.vars)) {
+    if (stop) {
+      stop(paste0("targets must contain ", length(model.vars), " numbers. ", length(targets),
+                  " were included."), call. = FALSE)
+    }
+    message(paste0("targets must contain ", length(model.vars), " numbers. ", length(targets),
+                   " were included.\nAll values in the output have been assigned NA."))
+    internal.targets <- setNames(rep(NA_real_, length(model.vars)), model.vars)
+  }
+  else {
+    internal.targets <- setNames(targets, model.vars)
+    if (!stop) {
+      if (any(attr(terms(formula), "order") > 1)) {
+        #message("targets look okay, but interactions were present in the formula, so make sure the order is correct.")
+      }
+      else {
+        #message("targets look okay, but you should print them to make sure they're correct.")
+      }
+    }
+  }
+
+  out <- internal.targets
+  class(out) <- "optweight.targets"
+  return(out)
+}
+
+print.optweight.targets <- function(x, digits = 5, ...) {
+  targets <- x
+  attributes(targets) <- NULL
+  names(targets) <- names(x)
+  if (all(is.na(targets))) {
+    cat("- vars:\n\t")
+    cat(paste(names(targets), collapse = "   "))
+  }
+  else {
+    cat("- targets:\n")
+    print(round(targets, digits))
   }
   invisible(x)
 }
