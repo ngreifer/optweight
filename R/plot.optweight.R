c7dd7cd11059a4d8651d9370f619f6a61760232b
Noah Greifer <noah.greifer@gmail.com>
2018-09-14 01:51:20 -0400

    Edits reflecting new duals structure

--- R/plot.optweight.R
+++ R/plot.optweight.R
@@ -3,26 +3,28 @@ plot.optweight <- function(x, which.time = 1, scale = c("var", "ess"), ...) {
     if (which.time %nin% seq_along(x$duals)) stop("which.time must correspond to an available time point.", call. = FALSE)
     d <- x$duals[[which.time]]
     title <- paste("Dual Variables for Balance Constraints at Time", which.time)
-    t <- x$treat.list[[which.time]]
+    #t <- x$treat.list[[which.time]]
   }
   else {
     d <- x$duals
     title <- "Dual Variables for Balance Constraints"
-    t <- x$treat
+    #t <- x$treat
   }
 
-  w <- x$weights
+  #w <- x$weights
   # if (scale == "ess") {
   #
   # }
-  duals <- data.frame(reshape(d, direction = "long", ids = rownames(d),
-                              idvar = "covs", varying = list(colnames(d)),
-                              timevar = "treat", times = colnames(d), v.names = "dual"))
+  # duals <- data.frame(reshape(d, direction = "long", ids = rownames(d),
+  #                             idvar = "covs", varying = list(colnames(d)),
+  #                             timevar = "treat", times = colnames(d), v.names = "dual"))
+  #
+  # duals$covs <- factor(duals$covs, levels = rev(unique(duals$covs)))
+  # duals$treat <- factor(duals$treat); levels(duals$treat) <- paste("Treat =", levels(duals$treat))
 
-  duals$covs <- factor(duals$covs, levels = rev(unique(duals$covs)))
-  duals$treat <- factor(duals$treat); levels(duals$treat) <- paste("Treat =", levels(duals$treat))
+  d$cov <- factor(d$cov, levels = rev(unique(d$cov)))
 
-  p <- ggplot(duals, aes(x = covs, y = dual)) +
+  p <- ggplot(d, aes(x = cov, y = dual)) +
     geom_col() +
     geom_hline(yintercept = 0) +
     coord_flip() +
@@ -32,6 +34,9 @@ plot.optweight <- function(x, which.time = 1, scale = c("var", "ess"), ...) {
     theme_bw() +
     scale_y_continuous(expand = expand_scale(c(0, .05)))
 
-  if (nlevels(duals$treat) > 1) p <- p + facet_grid(cols = vars(treat))
+  p <- p + facet_grid(cols = vars(treat),
+                                                  rows = vars(constraint),
+                                                  scales = "free_y", space = "free")
+  #else p <- p + facet_grid(rows = vars(constraint), scales = "free_y", space = "free")
   p
 }
