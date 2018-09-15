7b64e2862c775078e1695a884cf1f5dc293ed8e4
Noah Greifer <noah.greifer@gmail.com>
2018-09-15 14:30:20 -0400

    Cleaning

--- R/plot.optweight.R
+++ R/plot.optweight.R
@@ -1,42 +1,30 @@
-plot.optweight <- function(x, which.time = 1, scale = c("var", "ess"), ...) {
+plot.optweight <- function(x, which.time = 1, ...) {
   if ("optweightMSM" %in% class(x)) {
     if (which.time %nin% seq_along(x$duals)) stop("which.time must correspond to an available time point.", call. = FALSE)
     d <- x$duals[[which.time]]
-    title <- paste("Dual Variables for Balance Constraints at Time", which.time)
-    #t <- x$treat.list[[which.time]]
+    title <- paste("Dual Variables for Constraints at Time", which.time)
   }
   else {
     d <- x$duals
-    title <- "Dual Variables for Balance Constraints"
-    #t <- x$treat
+    title <- "Dual Variables for Constraints"
   }
 
-  #w <- x$weights
-  # if (scale == "ess") {
-  #
-  # }
-  # duals <- data.frame(reshape(d, direction = "long", ids = rownames(d),
-  #                             idvar = "covs", varying = list(colnames(d)),
-  #                             timevar = "treat", times = colnames(d), v.names = "dual"))
-  #
-  # duals$covs <- factor(duals$covs, levels = rev(unique(duals$covs)))
-  # duals$treat <- factor(duals$treat); levels(duals$treat) <- paste("Treat =", levels(duals$treat))
-
   d$cov <- factor(d$cov, levels = rev(unique(d$cov)))
+  d$treat <- factor(d$treat, levels = unique(d$treat), labels = paste("Treat:", unique(d$treat)))
+  d$constraint <- factor(d$constraint, levels = unique(d$constraint, nmax=2), labels = paste("Constraint:", unique(d$constraint, nmax=2)))
 
   p <- ggplot(d, aes(x = cov, y = dual)) +
     geom_col() +
     geom_hline(yintercept = 0) +
     coord_flip() +
-    labs(y = "Absolute Standardized Dual Variable",
+    labs(y = "Absolute Dual Variable",
          x = "Covariate",
          title = title) +
     theme_bw() +
     scale_y_continuous(expand = expand_scale(c(0, .05)))
 
   p <- p + facet_grid(cols = vars(treat),
-                                                  rows = vars(constraint),
-                                                  scales = "free_y", space = "free")
-  #else p <- p + facet_grid(rows = vars(constraint), scales = "free_y", space = "free")
+                      rows = vars(constraint),
+                      scales = "free_y", space = "free")
   p
 }
