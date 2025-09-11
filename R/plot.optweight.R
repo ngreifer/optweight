fe825551566ece195a0787514ab36ea15c049c5b
Noah Greifer <noah.greifer@gmail.com>
2025-09-11 16:02:24 -0400

    Cleaning, updates, crayon

--- R/plot.optweight.R
+++ R/plot.optweight.R
@@ -1,28 +1,23 @@
-#' Plot Dual Variables for Assessing Balance Constraints
+#' Plot Dual Variables for Covariate Constraints
 #'
-#' Plots the dual variables resulting from [optweight()] in a way similar to
-#' figure 2 of Zubizarreta (2015), which explained how to interpret these
-#' values. These represent the cost of changing the constraint on the variance
-#' of the resulting weights. For covariates with large values of the dual
-#' variable, tightening the constraint will increase the variability of the
-#' weights, and loosening the constraint will decrease the variability of the
-#' weights, both to a greater extent than would doing the same for covariate
-#' with small values of the dual variable.
+#' Plots the dual variables resulting from [optweight()], [optweightMV()], or [optweight.svy()] in a way similar to
+#' figure 2 of Zubizarreta (2015), which explains how to interpret these
+#' values.
 #'
-#' @param x an `optweight`, `optweightMV`, or `optweight.svy` object; the output of a
-#' call to [optweight()], [optweightMV()], or [optweight.svy()].
-#' @param which.treat For `optweightMV` objects, which treatment to display.
-#' Only one may be displayed at a time.
-#' @param \dots Ignored.
+#' @param x an `optweight`, `optweightMV`, or `optweight.svy` object; the output of a call to [optweight()], [optweightMV()], or [optweight.svy()].
+#' @param which.treat for `optweightMV` objects, an integer corresponding to which treatment to display. Only one may be displayed at a time.
+#' @param \dots ignored.
 #'
 #' @returns
 #' A `ggplot` object that can be used with other \pkg{ggplot2} functions.
 #'
-#' @seealso [optweight()], [optweightMV()], or [optweight.svy()] to estimate
-#' the weights and the dual variables
+#' @details
+#' Dual variables represent the cost of changing the constraint on the objective function minimized to estimate the weights. For covariates with large values of the dual variable, tightening the constraint will increase the variability of the weights, and relaxing the constraint will decrease the variability of the weights, both to a greater extent than would doing the same for covariate with small values of the dual variable. See [optweight()] and `vignette("optweight")` for more information on interpreting dual variables.
 #'
-#' [plot.summary.optweight()] for plots of the distribution of
-#' weights
+#' @seealso
+#' [optweight()], [optweightMV()], or [optweight.svy()] to estimate the weights and the dual variables.
+#'
+#' [plot.summary.optweight()] for plots of the distribution of weights.
 #'
 #' @references
 #' Zubizarreta, J. R. (2015). Stable Weights that Balance Covariates for Estimation With Incomplete Outcome Data. *Journal of the American Statistical Association*, 110(511), 910–922. \doi{10.1080/01621459.2015.1023805}
@@ -41,7 +36,7 @@
 #'                  tols = tols,
 #'                  estimand = "ATT")
 #'
-#' summary(ow1) # Note the RMSE Dev and effective
+#' summary(ow1) # Note the L2 divergence and effective
 #' #              sample size (ESS)
 #'
 #' plot(ow1) # age has a low value, married is high
@@ -71,72 +66,46 @@
 
 #' @exportS3Method plot optweight
 plot.optweight <- function(x, ...) {
-  d <- x$duals
   title <- "Dual Variables for Constraints"
 
-  d$cov <- factor(d$cov, levels = rev(unique(d$cov)))
-  d$constraint <- factor(d$constraint, levels = unique(d$constraint, nmax = 2L),
-                         labels = paste("Constraint:", unique(d$constraint, nmax = 2L)))
-
-  ggplot(d, mapping = aes(x = .data$cov, y = .data$dual)) +
-    geom_col() +
-    geom_hline(yintercept = 0) +
-    coord_flip() +
-    labs(y = "Absolute Dual Variable",
-         x = "Covariate",
-         title = title) +
-    theme_bw() +
-    scale_y_continuous(expand = expansion(c(0, .05))) +
-    facet_grid(rows = vars(.data$constraint),
-               scales = "free_y", space = "free")
+  .plot_optweight_internal(x$duals, title)
 }
 
 #' @rdname plot.optweight
 #' @exportS3Method plot optweightMV
-plot.optweightMV <- function(x, which.treat = 1, ...) {
+plot.optweightMV <- function(x, which.treat = 1L, ...) {
   chk::chk_count(which.treat)
 
   if (which.treat %nin% seq_along(x$duals)) {
     .err("`which.treat` must correspond to an available treatment")
   }
 
-  d <- x$duals[[which.treat]]
-  title <- sprintf("Dual Variables for Constraints for Treatment %s", which.treat)
+  title <- sprintf("Dual Variables for Constraints for Treatment %s",
+                   which.treat)
 
-  d$cov <- factor(d$cov, levels = rev(unique(d$cov)))
-  d$constraint <- factor(d$constraint, levels = unique(d$constraint, nmax = 2L),
-                         labels = paste("Constraint:", unique(d$constraint, nmax = 2L)))
-
-  ggplot(d, mapping = aes(x = .data$cov, y = .data$dual)) +
-    geom_col() +
-    geom_hline(yintercept = 0) +
-    coord_flip() +
-    labs(y = "Absolute Dual Variable",
-         x = "Covariate",
-         title = title) +
-    theme_bw() +
-    scale_y_continuous(expand = expansion(c(0, .05))) +
-    facet_grid(rows = vars(.data$constraint),
-               scales = "free_y", space = "free")
+  .plot_optweight_internal(x$duals[[which.treat]], title)
 }
 
 #' @rdname plot.optweight
 #' @exportS3Method plot optweight.svy
-plot.optweight.svy <- function(x, ...) {
-  d <- x$duals
-  title <- "Dual Variables for Target Constraints"
+plot.optweight.svy <- plot.optweight
 
+.plot_optweight_internal <- function(d, title) {
   d$cov <- factor(d$cov, levels = rev(unique(d$cov)))
 
-  p <- ggplot(d, mapping = aes(x = .data$cov, y = .data$dual)) +
-    geom_col() +
-    geom_hline(yintercept = 0) +
-    coord_flip() +
-    labs(y = "Absolute Dual Variable",
-         x = "Covariate",
-         title = title) +
-    theme_bw() +
-    scale_y_continuous(expand = expansion(c(0, .05)))
+  constraint_types <- unique(d$constraint, nmax = 2L)
 
-  p
+  d$constraint <- factor(d$constraint,
+                         levels = constraint_types,
+                         labels = paste("Constraint:", constraint_types))
+
+  ggplot(d) +
+    geom_col(aes(y = .data$cov, x = .data$dual)) +
+    scale_x_continuous(expand = expansion(c(0, .05))) +
+    facet_grid(rows = vars(.data$constraint),
+               scales = "free_y", space = "free") +
+    labs(x = "Absolute Dual Variable",
+         y = "Variable",
+         title = title) +
+    theme_bw()
 }
--- R/process_targets.R
+++ R/process_targets.R
@@ -1,18 +1,17 @@
 #' Construct and Check Targets Input
 #'
-#' Checks whether proposed target population means values for `targets` are suitable in number and order for submission to [optweight()] and [optweight.svy()]. Users should include one value per variable in `formula`. For factor variables, one value per level of the variable is required. The output of `process_targets()` can also be used as an input to `targets` in [optweight()] and [optweight.svy()].
+#' Checks whether proposed target population means values for `targets` are suitable in number and order for submission to [optweight()], [optweightMV()], and [optweight.svy()], and returns an object that can supplied to the `targets` argument of these functions.
 #'
+#' @inheritParams optweight.svy
 #' @inheritParams process_tols
-#' @param formula a formula with the covariates to be targeted on the right-hand side. See [glm()] for more details. Interactions and functions of covariates are allowed. Can be omitted, in which case all variables in `data` are assumed targeted.
 #' @param targets a vector of target population mean values for each covariate. These should be in the order corresponding to the order of the corresponding variable in `formula`, except for interactions, which will appear after all lower-order terms. For factor variables, a target value must be specified for each level of the factor, and these values must add up to 1. If `NULL`, the current sample means will be produced (weighted by `s.weights`). If `NA`, an `NA` vector named with the covariate names will be produced.
-#' @param s.weights an optional vector of sampling weights. Default is a vector of 1s.
 #' @param x an `optweight.targets` object; the output of a call to `process_targets()`.
 #'
 #' @returns
-#' An `optweight.targets` object, which is a named vector of target population mean values, one for each (expanded) covariate specified in `formula`. This should be used as user inputs to [optweight()] and [optweight.svy()].
+#' An `optweight.targets` object, which is a named vector of target population mean values, one for each (expanded) covariate specified in `formula`. This should be used as an input to the `targets` argument of [optweight()], [optweightMV()], and [optweight.svy()].
 #'
 #' @details
-#' The purpose of `process_targets()` is to allow users to ensure that their proposed input to `targets` in [optweight()] and [optweight.svy()] is correct both in the number of entries and their order. This is especially important when factor variables and interactions are included in the formula because factor variables are split into several dummies and interactions are moved to the end of the variable list, both of which can cause some confusion and potential error when entering `targets` values.
+#' The purpose of `process_targets()` is to allow users to ensure that their proposed input to `targets` in [optweight()], [optweightMV()], and [optweight.svy()] is correct both in the number of entries and their order. This is especially important when factor variables and interactions are included in the formula because factor variables are split into several dummies and interactions are moved to the end of the variable list, both of which can cause some confusion and potential error when entering `targets` values.
 #'
 #' Factor variables are internally split into a dummy variable for each level, so the user must specify a target population mean value for each level of the factor. These must add up to 1, and an error will be displayed if they do not. These values represent the proportion of units in the target population with each factor level.
 #'
@@ -187,11 +186,11 @@ print.optweight.targets <- function(x, digits = 5, ...) {
   names(targets) <- names(x)
 
   if (all(is.na(targets))) {
-    cat(sprintf("- vars:\n\t%s",
-                paste(names(targets), collapse = space(3L))))
+    cat0(" - ", .it("variables"), ":\n\t",
+         paste(names(targets), collapse = space(3L)))
   }
   else {
-    cat("- targets:\n")
+    cat0(" - ", .it("targets"), ":\n")
     print(round(targets, digits))
   }
 
